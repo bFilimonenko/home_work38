4aeba55f0ff020d395e0537a0de97d0a2eb181f6
bogdanafilimonenko <bfilimonenko@gmail.com>
2025-03-10 16:01:12 +0200

    feature: add styles, combined components, add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-// import { useState } from "react";
 import "./App.css";
 import { Button } from "./components/Button.jsx";
 import { Input } from "./components/Input.jsx";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const greet = () => {
+    !name ? alert(`You didn't enter a name`) : alert(`Hello, ${name}!`);
+  };
+
   return (
     <>
-      <Input/>
-      <Button></Button>
+      <p>Your name is {name || "..."}?</p>
+      <Input onChange={e =>setName(e.target.value)} placeholder={"Enter Your Name"}/>
+      <Button text={"Save"} onClick={greet}></Button>
     </>
   );
 }
